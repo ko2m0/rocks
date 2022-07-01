15624c3b5bc2a6caf133f2cd6e40dce4efc6da71
Eduardo Romero <kozmo@Eduardos-iMac.local>
2022-07-01 14:07:29 -0500

    updated router dom2

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './App.css';
 import Navbar  from './components/Navbar';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import Home from './components/pages/HomePage/Home';
 import Experience from './components/pages/Experience/Experience';
 import Education from './components/pages/Education/Education';
@@ -11,16 +11,16 @@ import NoPage from './components/pages/NoPage';
 
 function App() {
   return (
-      <Router>
+      <BrowserRouter>
         <Navbar/>
         <Switch>
-          <Route exact path='/' index><Home/></Route>
+          <Route exact path='/'><Home/></Route>
           <Route path='/experience' component={Experience}/>
           <Route path='/education' component={Education}/>
           <Route path="*" component={NoPage} />
         </Switch>
         <Footer/>
-      </Router>
+      </BrowserRouter>
       
   );
 }
