fb1a1aa4ba597dd75c818e3a15b7c5a6190afd9b
Eduardo Romero <kozmo@Eduardos-iMac.local>
2022-07-20 13:26:33 -0500

    Updates

---
+++ precache-manifest.8394c7186d1774cd1f96570537006525.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "26cc6bec832c8a72f86f",
+    "url": "/rocks/static/css/main.4dc2a7e2.chunk.css"
+  },
+  {
+    "revision": "26cc6bec832c8a72f86f",
+    "url": "/rocks/static/js/main.5cad31f5.chunk.js"
+  },
+  {
+    "revision": "1493ce6d85b91c19b9b1",
+    "url": "/rocks/static/js/runtime~main.0c4fb4b5.js"
+  },
+  {
+    "revision": "04ac5070f8f8056bed65",
+    "url": "/rocks/static/js/2.e38754df.chunk.js"
+  },
+  {
+    "revision": "f061aee63892179ccbe167d293b4a344",
+    "url": "/rocks/static/media/blot_1.f061aee6.png"
+  },
+  {
+    "revision": "e9e0664cb6f23f0e7807a1c45c67e362",
+    "url": "/rocks/static/media/retro_computer_personal_use-webfont.e9e0664c.woff"
+  },
+  {
+    "revision": "1e40420a18ac9f082d633b144f36b3cf",
+    "url": "/rocks/static/media/retro_computer_personal_use.1e40420a.ttf"
+  },
+  {
+    "revision": "37a5cf4adc1ef26d8593343590b4bd34",
+    "url": "/rocks/index.html"
+  }
+];
